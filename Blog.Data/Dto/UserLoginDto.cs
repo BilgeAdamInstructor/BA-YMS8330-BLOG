1cc8471a918b298465c4fc84c9003151cb459165
Erçin Dedeoğlu <e.dedeoglu@gmail.com>
2019-11-27 21:31:50 +0300

    27.11.2019

---
+++ Blog.Data/Dto/UserLoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Data.Dto
+{
+    public class UserLoginDto
+    {
+        [Required, MinLength(6), MaxLength(345)]
+        public string Email { get; set; }
+
+        [Required, MinLength(8), MaxLength(32)]
+        public string Password { get; set; }
+    }
+}
