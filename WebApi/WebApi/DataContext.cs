836b3abfab8052c8aebf25404d0fff16f0ae6052
Erçin Dedeoğlu <e.dedeoglu@gmail.com>
2019-12-24 19:35:27 +0300

    WebApi

---
+++ WebApi/WebApi/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.User>().HasData(new Models.User()
+            {
+                Id = 1,
+                Age = 1,
+                Username = "Test1234"
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Models.User> Users { get; set; }
+    }
+}
